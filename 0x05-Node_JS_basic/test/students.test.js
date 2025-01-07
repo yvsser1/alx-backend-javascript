d87719080361dee3eb51dd46e70b2987afb94d53
Yasser <yasserdalouzi@gmail.com>
2025-01-07 12:07:06 +0100

    fix linting errors

--- 0x05-Node_JS_basic/test/students.test.js
+++ 0x05-Node_JS_basic/test/students.test.js
@@ -1,20 +1,21 @@
-import app from '../full_server/server.js';
 import { use, request, expect } from 'chai';
 import chaiHttp from 'chai-http';
+import app from '../full_server/server.js';
+
 process.argv[2] = './database.csv';
 
 use(chaiHttp);
 
-describe('Server!', () => {
-  it('welcomes user to the api', (done) => {
+describe('server!', () => {
+  it('welcomes user to the api', () => new Promise((done) => {
     request(app)
       .get('/students/CS')
       .end((err, res) => {
         expect(res).to.have.status(200);
         expect(res.text).to.equals(
-          'List: Johann, Arielle, Jonathan, Emmanuel, Guillaume, Katie'
+          'List: Johann, Arielle, Jonathan, Emmanuel, Guillaume, Katie',
         );
         done();
       });
-  });
+  }));
 });
